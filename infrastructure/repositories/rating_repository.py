3948e0d805b6416c5d096f1856009eb73e993e8f
Thalys M <thalyscorreia@hotmail.com>
2025-04-03 19:43:32 -0300

    fix: mudando busca de informacoes

--- infrastructure/repositories/rating_repository.py
+++ infrastructure/repositories/rating_repository.py
@@ -10,6 +10,3 @@ class RatingRepository:
 
     def get_ratings_for_movie(self, movie_id: int):
         return list(self.collection.find({"movie_id": movie_id}, {"_id": 0}))
-
-    def get_user_ratings(self, user_id: str):
-        return list(self.collection.find({"user_id": user_id}, {"_id": 0}))
