f24b87a2f4657002d27b57eb923da7eb72c84f51
Thalys M <thalyscorreia@hotmail.com>
2025-04-08 16:48:56 -0300

    feat: randomiza as recomendações para variar o resultado

--- infrastructure/services/recommendation_engine.py
+++ infrastructure/services/recommendation_engine.py
@@ -2,16 +2,18 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy.sparse import load_npz
+import random
 
 class RecommendationEngine:
     def __init__(self):
         self.df = pd.read_csv("movies_processed.csv")
         self.matrix = load_npz("combined_matrix.npz")
 
+
     def recommend(self, user1_movies, user2_movies, top_n=5):
         indices = self.df[self.df["original_title"].isin(user1_movies + user2_movies)].index.tolist()
 
-        print(user1_movies,user2_movies)
+        print(user1_movies, user2_movies)
         if not indices:
             return []
 
@@ -21,9 +23,20 @@ class RecommendationEngine:
         similarities = cosine_similarity([user_vector], self.matrix)[0]
         self.df["similarity"] = similarities
 
-        recommendations = self.df[~self.df["original_title"].isin(user1_movies + user2_movies)].sort_values(by="similarity", ascending=False).head(top_n)
+        # Remove os filmes já vistos
+        unseen_df = self.df[~self.df["original_title"].isin(user1_movies + user2_movies)]
 
-        return recommendations[["original_title", "similarity","id","poster_path"]].to_dict(orient="records")
+        # Pega os top 25 mais similares
+        top_candidates = unseen_df.sort_values(by="similarity", ascending=False).head(50)
+
+        # Embaralha levemente
+        top_candidates = top_candidates.sample(frac=1.0, random_state=random.randint(0, 9999))
+
+        # Seleciona os top_n finais
+        recommendations = top_candidates.head(top_n)
+
+        return recommendations[["original_title", "similarity", "id", "poster_path"]].to_dict(orient="records")
+
 
 if __name__ == "__main__":
     engine = RecommendationEngine()
