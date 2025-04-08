51f6df1e1af757a82ab0a47b399dcaa1014e69d9
Thalys M <thalyscorreia@hotmail.com>
2025-04-08 15:51:29 -0300

    refactor: utilizando fuzz para incrementar as buscas

--- infrastructure/repositories/movie_repository.py
+++ infrastructure/repositories/movie_repository.py
@@ -1,4 +1,5 @@
 from core.database import get_collection
+from rapidfuzz import fuzz
 
 class MovieRepository:
     def __init__(self):
@@ -12,5 +13,29 @@ class MovieRepository:
         return self.collection.find_one({"id": movie_id}, {"_id": 0})
     
     def get_movie_by_name(self, movie_name: str):
-        movies = self.collection.find({"title": {"$regex": movie_name, "$options": "i"}}, {"_id": 0})
-        return list(movies)
+        movies_cursor = self.collection.find(
+            {"title": {"$regex": movie_name, "$options": "i"}}, {"_id": 0}
+        )
+        
+        movies = list(movies_cursor)
+
+        if not movies:
+            return []
+
+        # Calcula a similaridade e filtra por score mínimo
+        scored_movies = [
+            (movie, fuzz.partial_ratio(movie.get("original_title", ""), movie_name))
+            for movie in movies
+        ]
+        filtered = [movie for movie, score in scored_movies if score >= 70]
+
+        # Ordena por maior similaridade e limita a 12 resultados
+        ranked = sorted(
+            filtered,
+            key=lambda movie: fuzz.partial_ratio(movie.get("title", ""), movie_name),
+            reverse=True
+        )
+
+        return ranked[:12]
+    
+
