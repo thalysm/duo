a6cd518db35ec76fae8d1a1ab8d7801815bb1e1c
Thalys M <thalyscorreia@hotmail.com>
2025-04-24 13:00:30 -0300

    feat: recebendo os ids dos filmes nos rating inves de nomes

--- domain/entities/rating.py
+++ domain/entities/rating.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel, Field
+from typing import List
 
 class Rating(BaseModel):
     movie_id: int
     rating: float = Field(..., ge=0, le=10)  # Avaliação entre 0 e 10
-    movies_associated: list = []
+    movies_associated: List[float]
     similarity: float = 0.0
